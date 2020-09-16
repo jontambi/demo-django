d0d6d2440bdb0c80798c5c52fb05a88f00a44653
John Tambi <jon.tambi@gmail.com>
2020-09-15 23:11:35 -0500

    first commit

---
+++ refresh/refresh.py
@@ -0,0 +1,13 @@
+name = "Julie"
+
+age = "42"
+
+sentence = "Hi my name is {} and I am {} years old".format(name, age)
+
+print(sentence)
+
+years = 2008
+if 2000 < years < 2099:
+    print("Welcome to the 21st century!")
+else:
+    print("You are before or after the 21st century")
