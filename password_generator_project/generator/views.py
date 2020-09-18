a3e75e90dcd55920f98724590923351936ed65ee
John Tambi <jon.tambi@gmail.com>
2020-09-17 19:41:54 -0500

    add new features

--- password_generator_project/generator/views.py
+++ password_generator_project/generator/views.py
@@ -1,6 +1,7 @@
 from django.http import request
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def home(request):
@@ -8,4 +9,21 @@ def home(request):
     #return HttpResponse('Hello there friend')
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&+'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+
+    length = int(request.GET.get('lenght', 16))
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': thepassword})
